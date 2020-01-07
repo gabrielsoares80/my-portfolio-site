69ca808b66386885cf8ed40b4a5a553111a3b573
Gabriel Roth Soares <47960725+gabrielsoares80@users.noreply.github.com>
2020-01-07 12:58:40 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,18 @@
 require('dotenv').config()
 
-export const siteMetadata = {
-  title: `Creative Portfolio`,
-}
-export const plugins = [
-  `gatsby-plugin-react-helmet`,
-  `gatsby-plugin-sass`,
-  `gatsby-transformer-remark`,
-  {
-    resolve: `gatsby-source-datocms`,
-    options: {
-      apiToken: process.env.DATO_API_TOKEN,
-    },
+module.exports = {
+  siteMetadata: {
+    title: `Creative Portfolio`,
   },
-]
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: process.env.DATO_API_TOKEN,
+      },
+    },
+  ],
+}
